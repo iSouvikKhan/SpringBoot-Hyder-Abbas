a876088fa6977a6cb9d0d1b2fd853a6a91a86af0
isouvikkhan <souvik97381@gmail.com>
2025-08-16 01:18:37 +0530

    crm app with Thymleaf

---
+++ Spring_MVC/WebbAPPCRMThymleaf/src/main/java/com/telusko/WebbAPPCRM/WebbAppcrmApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.WebbAPPCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbAppcrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbAppcrmApplication.class, args);
+	}
+
+}
